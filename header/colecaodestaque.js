548a75c0879eab81f0af866a748e455cb3f06da4
janinynobrega <janinynobrega@gmail.com>
2025-05-06 11:15:36 -0300

    inicio do segundo array para a inserção de cards da section coleções em destaques

---
+++ header/colecaodestaque.js
@@ -0,0 +1,28 @@
+const colecaoDestaque = [
+    {
+        imagemSrc: "./img-colecaoDestaque/camisa.svg",
+        altImagem: "imagem de um traço de uma camisa",
+        titulo: "Camisetas"
+    },
+    {
+        imagemSrc: "./img-colecaoDestaque/calça.svg",
+        altImagem: "imagem de um traço de uma calça",
+        titulo: "Calças"
+    },
+    {
+        imagemSrc: "./img-colecaoDestaque/calça.svg",
+        altImagem: "imagem de um traço de uma camisa",
+        titulo: "Bonés"
+    },
+    {
+        imagemSrc: "./img-colecaoDestaque/Vector.svg",
+        altImagem: "imagem de um traço de um heaphone",
+        titulo: "Headphones"
+    },
+    {
+        imagemSrc: "./img-colecaoDestaque/tenis.svg",
+        altImagem: "imagem de um traço de um tenis",
+        titulo: "Tênis"
+    }
+];
+
